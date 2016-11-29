0b3601fe2c31c9f75c3387e059f586b76313694d
Edmund Wong <wongkokliong99@hotmail.com>
2016-11-30 04:46:19 +0800

    Working on git branches

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,2 @@
-var gulp = require('gulp'),
-    watch = require('gulp-watch'),
-    postcss = require('gulp-postcss'),
-    autoprefixer = require('autoprefixer'),
-    cssvars = require('postcss-simple-vars'),
-    nested = require('postcss-nested');
-
-gulp.task('styles', function() {
-return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssvars, nested, autoprefixer]))
-    .pipe(gulp.dest('./app/temp/.css'));
-});
-
-gulp.task('watch', function() {
-
-
-    watch('./app/assets/styles/**/*.css', function() {
-        gulp.start('styles');
-    });
-
-});
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
